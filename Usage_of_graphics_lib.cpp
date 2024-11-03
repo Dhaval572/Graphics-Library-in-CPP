b8295d193ec049877133fd64aa4f60d849ea94de
Dhaval572 <dp9718776@gmail.com>
2024-11-03 14:38:14 +0530

    Create Usage_of_graphics_lib.cpp

---
+++ Usage_of_graphics_lib.cpp
@@ -0,0 +1,27 @@
+// Usage of graphics library
+
+/*
+
+Uses:
+
+    In very simple and short way we can say that graphics lib is used to make drawing, animation and simple 2d games in c++.
+
+    This library can help you in made mini projects like draw star, make amimation of moving car, and make simple games like snake game etc.
+
+    This library can help you to build a logic for game programming.
+
+    So, it has many advantages so if you want to learn this than see tutorials in youtube.
+
+    Here, is recommanded channel for learn graphics.h in c++
+
+    https://www.youtube.com/watch?v=jcGNtKTTHys&list=PLMclp-q8OPhnBqdI6DAIhsHX6RvVD5nha
+
+    This playlist is very very usefull for beginners to learn graphics lib 
+
+    And all videos of this channel is in hindi language.
+
+    So, keep learning and exploring! 
+
+    Also enjoy coding!👨‍💻
+
+*/
