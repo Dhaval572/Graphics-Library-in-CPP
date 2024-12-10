aa2e056256adf653ab7cd8c701ea64e2c463e4bf
Dhaval572 <dp9718776@gmail.com>
2024-12-10 16:42:52 +0530

    Update Color_functions_in_graphics_lib_Part-1.cpp

--- Color_functions_in_graphics_lib_Part-1.cpp
+++ Color_functions_in_graphics_lib_Part-1.cpp
@@ -13,6 +13,11 @@ int main()
     setbkcolor(MAGENTA); // Setting given color to background
     cleardevice();       // Don't forget this other wise background color changes will not visible 
 
+    // You can also use this rgb function for get other color 
+
+    int rgbColor = COLOR(49, 8, 64);
+    clearviewport(); // To apply background color
+
     getch();
     closegraph();
 }
@@ -32,6 +37,14 @@ Color:
 
     This all color has own index cause of enum.
 
+    If you are want to add another colors than you can use COLOR() which is uses rgb colors.
+
+    Syntax:
+
+        COLOR( value_for_red, Value_for_green, Value_for_blue );
+
+        Example: COLOR(49, 8, 64);
+
     Functions of colors in graphics lib:
 
         1) getmaxcolor()
@@ -47,7 +60,7 @@ Color:
             This function is used to set background color.
             It takes one argument which is name of color. 
 
-            After use this you must have to call this function also `cleardevice()` if you don't use this than background color will not visible.
+            After use this you must have to call this function also `clearviewport()` or `cleardevice()` if you don't use this than background color will not visible.
 
     So, this was some color functions which is used in graphics library
     ( Note: this is part 1 so part 2 is also available check in my repo )
